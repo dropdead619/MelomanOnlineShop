6113173ec91d91239a6ef6b819fcf77e6274f6ac
Demezhan Kazhkenov <59953572+dropdead619@users.noreply.github.com>
2021-02-27 15:19:40 +0600

    Added Twilio Service

---
+++ Services/Abstract/IMessageService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Abstract
+{
+    public interface IMessageService
+    {
+        public void Send(string number);
+    }
+}
---
+++ Services/RandomCodeGenerationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Services
+{
+    public class RandomCodeGenerationService
+    {
+        public static string Generate(int length)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[length];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            return finalString;
+        }
+    }
+}
